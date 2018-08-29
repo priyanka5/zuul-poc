0015f7d1bbd58ef8040ee3843cd40a5ac1e8c771
aleMessoMale <ale.casula@gmail.com>
2018-08-29 14:54:25 +0200

    decommented zuul filter for unirest calls

--- src/main/java/com/example/apigateway/configuration/ZuulFiltersConfig.java
+++ src/main/java/com/example/apigateway/configuration/ZuulFiltersConfig.java
@@ -17,11 +17,11 @@ public class ZuulFiltersConfig {
         return new EnrichmentFilter();
     }
 
-	/* Decomment this Bean for make it works into openshift
+	/* Decomment this Bean for make it works into openshift*/
 	
     @Bean
     public UniRestSimpleHostRoutingFilter simpleHostRoutingFilter(ProxyRequestHelper proxyRequestHelper, ZuulProperties zuulProperties) {
         return new UniRestSimpleHostRoutingFilter(proxyRequestHelper,zuulProperties,null);
     }
-	*/
+	
 }
